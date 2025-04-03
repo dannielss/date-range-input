effe26deee9a2d751ef4611d22539b01203ff56e
Daniel Sousa <daniel_sousa.p@outlook.com>
2025-04-03 02:17:34 -0300

    chore: add dependencies and configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { babel } from "@rollup/plugin-babel";
+
+export default {
+    input: "src/index.ts",
+    output: [
+        { file: "dist/index.js", format: "cjs", sourcemap: true },
+        { file: "dist/index.esm.js", format: "esm", sourcemap: true }
+    ],
+    plugins: [
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: "./tsconfig.json" }),
+        babel({ babelHelpers: "bundled" })
+    ],
+    external: ["react", "react-dom"]
+};
